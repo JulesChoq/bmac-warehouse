68ac45f3510211411679a3e12822f7a55db021c0
Jules Choquart <julochoq@gmail.com>
2018-10-09 17:35:32 -0700

    moved function to component and added verifier button

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -6,47 +6,17 @@ import React from 'react';
 import firebase from '../../firebase.js';
 import { Button } from 'antd' ;
 
-var provider = new firebase.auth.GoogleAuthProvider();
-firebase.auth().languagecode = 'us';
-var user = firebase.auth().currentUser;
-var name, email, photoURL, uid, emailVerified;
-
- if(user!=null) {
-      name = user.displayName;
-      email = user.email;
-    }
 const styles = {
-  container: {
+  container: { 
+    display: 'flex',
+    justifyContent: 'space-between',
+    margin: '5%',
+    
   },
 };
 
-
-firebase.auth().getRedirectResult().then(function(result) {
-  if (result.credential) {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    var token = result.credential.accessToken;
-    // ...
-  }
-  // The signed-in user info.
-
-  var user = firebase.auth().currentUser;
-  
-
-  var user = result.user;  
-}).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // The email of the user's account used.
-  var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  var credential = error.credential;
-  // ...
-});
-
-firebase.auth().signOut().then(function() {
-}).catch(function(error) {
-});
+var provider = new firebase.auth.GoogleAuthProvider();
+firebase.auth().languagecode = 'us';
 
 class Login extends React.Component {
   constructor(props){
@@ -56,12 +26,45 @@ class Login extends React.Component {
     }
   }
 
+  signIn = () => {
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      // ...
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
+}
+  
+  signOut = () => {firebase.auth().signOut().then(function() {
+  }).catch(function(error) {
+  });
+}
+
+  getUser = () => {firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      alert("User is signed in.")
+    } else {
+       alert("User is not signed in.")
+    }
+  })
+  }
+
   render() {
     return(
       <div style={styles.container}>
-      <Button onClick={()=>{firebase.auth().signInWithRedirect(provider);}} type="primary">Login</Button>
-      {/*<Button onClick={()=>{firebase.auth().signOut(provider)}} type="danger">Logout</Button>*/}
-      {user?user.email:"Not logged in"}
+      <Button  onClick={this.signIn} type="primary">Login</Button>
+      <Button onClick={this.getUser} type ="outline">Check User</Button>
+      <Button  onClick={this.signOut} type="danger">Logout</Button>
       </div>
 
     );
