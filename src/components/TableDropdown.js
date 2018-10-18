c1ba0633bea6d96a27c9e4e1cd8fe49df54f1351
Paul Milloy <milloypr@whitman.edu>
2018-10-18 11:05:45 -0700

    checkpoint

--- src/components/TableDropdown.js
+++ src/components/TableDropdown.js
@@ -1,54 +1,75 @@
 /**
  *  A component
  */
+import React from "react";
+import { render } from "react-dom";
+import { makeData, Logo, Tips } from "./Utils";
 
-import React from 'react';
-import ReactTable from 'react-table';
+// Import React Table
+import ReactTable from "react-table";
 import "react-table/react-table.css";
 
-const styles = {
-  container: {
-  },
-};
-
-    const data = [
-      {
-        name: 'Ben Limpenis',
-        age: 12,
-      },
-      {
-        name: 'Jules Chopenis',
-        age: 8,
-      },
-      {
-        name: 'Rajesh Narapenis',
-        age: 70,
-      }      
-    ];
-
-  const columns = [{
-    Header: 'Name',
-    accessor: 'name'
-  }, {
-    Header: 'Age',
-    accessor: 'age'
-  }]
-
-class TableDropdown extends React.Component {
-  constructor(props){
-    super(props);
+class App extends React.Component {
+  constructor() {
+    super();
     this.state = {
-    }
+      data: makeData()
+    };
+    this.renderEditable = this.renderEditable.bind(this);
   }
-
+  renderEditable(cellInfo) {
+    return (
+      <div
+        style={{ backgroundColor: "#fafafa" }}
+        contentEditable
+        suppressContentEditableWarning
+        onBlur={e => {
+          const data = [...this.state.data];
+          data[cellInfo.index][cellInfo.column.id] = e.target.innerHTML;
+          this.setState({ data });
+        }}
+        dangerouslySetInnerHTML={{
+          __html: this.state.data[cellInfo.index][cellInfo.column.id]
+        }}
+      />
+    );
+  }
   render() {
-    return(
-      <ReactTable
-        data={data}
-        columns={columns}>
-      </ReactTable>
+    const { data } = this.state;
+    return (
+      <div>
+        <ReactTable
+          data={data}
+          columns={[
+            {
+              Header: "First Name",
+              accessor: "firstName",
+              Cell: this.renderEditable
+            },
+            {
+              Header: "Last Name",
+              accessor: "lastName",
+              Cell: this.renderEditable
+            },
+            {
+              Header: "Full Name",
+              id: "full",
+              accessor: d =>
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: d.firstName + " " + d.lastName
+                  }}
+                />
+            }
+          ]}
+          defaultPageSize={10}
+          className="-striped -highlight"
+        />
+        <br />
+        <Tips />
+        <Logo />
     );
   }
 }
 
-export default TableDropdown;
+
