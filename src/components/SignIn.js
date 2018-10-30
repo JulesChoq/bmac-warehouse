e0dea0cd5b8367abd0434ff0e220278923660c95
Pablo Fernandez <contact@pablofernandez.com>
2018-10-29 19:50:41 -0700

    ee

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -5,11 +5,17 @@ import { auth } from '../firebase';
 import * as routes from '../constants/routes';
 import firebase from '../firebase';
 import { Form, Icon, Input, Button, Checkbox } from 'antd';
-
+import './SignIn.css';
 
 const SignInPage = ({ history }) =>
   <div>
-    <h1>SignIn</h1>
+    <br/><br/>
+    <h1>User SignIn</h1>
+    <p>
+    Access to BMAC Warehouse requires a Username and a Password.<br/>
+    You must be a BMAC staff member to access this system.
+    <br/>
+    </p>
     <SignInForm history={history} />
     <SignUpLink />
   </div>
@@ -69,25 +75,30 @@ class SignInForm extends Component {
       email === '';
 
     return (
+      <div className="login-form">
       <Form onSubmit={this.onSubmit}>
         <Input
+          prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
           value={email}
           onChange={event => this.setState(byPropKey('email', event.target.value))}
           type="text"
           placeholder="Email Address"
         />
         <Input
+          prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
           value={password}
           onChange={event => this.setState(byPropKey('password', event.target.value))}
           type="password"
           placeholder="Password"
         />
-        <Button disabled={isInvalid} onClick={this.onClick} type="primary">
+        <br/><br/>
+        <Button disabled={isInvalid} onClick={this.onClick} type="primary" className="login-form-button">
           Sign In
         </Button>
 
         { error && <p>{error.message}</p> }
       </Form>
+      </div>
     );
   }
 }
